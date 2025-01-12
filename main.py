12635101838340612843cf387051306283df2a47
GoodnessA1 <goodnessabisoye@gmail.com>
2025-01-12 15:34:32 +0100

    Made some corrections

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import openai
 from openai import OpenAI
+import os
 
 def chatbot(message):
     base_url = "https://api.aimlapi.com/v1"
-    api_key = "43cca4d19363433f8fac5325ad0745e6"
+    api_key = os.getenv('API_KEY')
     api = OpenAI(base_url=base_url, api_key=api_key)
 
     completion = api.chat.completions.create(
